e30a0b92a0df768a49798a7539e89eb7374da75f
ajanb <ajanbalaganesh@cmail.carleton.ca>
2025-10-26 20:31:00 -0400

    Added rank enum

---
+++ src/Rank.java
@@ -0,0 +1,7 @@
+public enum Rank {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE, DRAW_TWO, WILD, WILD_DRAW_TWO;
+
+    public boolean isNumber() {
+        return ordinal() <= NINE.ordinal();
+    }
+}
